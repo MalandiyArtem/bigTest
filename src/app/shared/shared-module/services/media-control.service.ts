24ccc9ef3ed5a75172543c0d9fe6004b4aa67b16
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-22 15:51:28 +0200

    sdfjkhsdf

--- src/app/shared/shared-module/services/media-control.service.ts
+++ src/app/shared/shared-module/services/media-control.service.ts
@@ -10,6 +10,9 @@ export class MediaControlService {
   setIsMouseOnTheDocument(isMouseOnDocument: boolean) {
     this.isMouseOnDocument$.next(isMouseOnDocument);
   }
+  1 2 3 4 5 6 7 8 9 0 10 12 11 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+
+  31 32 33 34 35 356 
 
   getIsMouseOnDocument() {
     return true || false;
