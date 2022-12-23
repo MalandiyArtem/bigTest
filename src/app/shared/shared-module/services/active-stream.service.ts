5f46a5cbb62e65060b1e478dc31f1628b3e8e9e5
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-23 14:43:28 +0200

    end

--- src/app/shared/shared-module/services/active-stream.service.ts
+++ src/app/shared/shared-module/services/active-stream.service.ts
@@ -1,4 +1,4 @@
-// import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';test
 // import { Guid } from 'guid-typescript';
 // import { ReplaySubject } from 'rxjs';
 // import { StreamType } from '../../enums/stream-type';
