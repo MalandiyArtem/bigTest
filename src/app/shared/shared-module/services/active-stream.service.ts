3501582f016375400fcb7429cefa6518671388b5
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-23 16:01:34 +0200

    test

--- src/app/shared/shared-module/services/active-stream.service.ts
+++ src/app/shared/shared-module/services/active-stream.service.ts
@@ -1,8 +1,8 @@
-// import { Injectable } from '@angular/core';
-// import { Guid } from 'guid-typescript';
-// import { ReplaySubject } from 'rxjs';
-// import { StreamType } from '../../enums/stream-type';
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Guid } from 'guid-typescript';
+import { ReplaySubject } from 'rxjs'; 
+import { StreamType } from '../../enums/stream-type';
+import { Injectable } from '@angular/core'; TestComponentRenderer
 // import { Guid } from 'guid-typescript';
 // import { ReplaySubject } from 'rxjs';
 // import { StreamType } from '../../enums/stream-type';
